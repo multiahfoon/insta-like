d311ee14966a47e1ea70c735e1767f2685e47375
multiahfoon <multiahfoon@gmail.com>
2022-07-14 12:48:26 +1200

    feat: add home  icon

--- components/Header.js
+++ components/Header.js
@@ -7,6 +7,7 @@ import {
   SearchIcon,
   UserGroupIcon,
 } from '@heroicons/react/outline'
+import { HomeIcon } from '@heroicons/react/solid'
 
 export function Header() {
   return (
@@ -29,20 +30,23 @@ export function Header() {
         />
       </div>
 
-      {/* mid - search bar*/}
-      <div className='relative mt-1 p-3 rounded-md'>
-        <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
-          <SearchIcon className='h-5 w-5 text-gray-500 ' />
-        </div>
+      <div className='max-w-xs'>
+        {/* mid - search bar*/}
+        <div className='relative mt-1 p-3 rounded-md'>
+          <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
+            <SearchIcon className='h-5 w-5 text-gray-500 ' />
+          </div>
 
-        <input
-          className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md'
-          placeholder='Search'
-          type='text'
-        />
+          <input
+            className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md'
+            placeholder='Search'
+            type='text'
+          />
+        </div>
       </div>
 
       {/* right */}
+      <HomeIcon className='h-10 w-10' />
     </div>
   )
 }
