bf1616f1dfc7620b7e199809587bdb5cf5d996a6
multiahfoon <multiahfoon@gmail.com>
2022-07-14 16:21:14 +1200

    style: convert components to functions

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,7 +1,6 @@
-import { NextComponentType } from 'next'
 import { Stories } from './Stories'
 
-export const Feed: NextComponentType = () => {
+export function Feed() {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl max-auto'>
       <section className='col-span-2'>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,3 @@
-import { NextComponentType } from 'next'
 import Image from 'next/image'
 import {
   HeartIcon,
@@ -10,7 +9,7 @@ import {
 } from '@heroicons/react/outline'
 import { HomeIcon } from '@heroicons/react/solid'
 
-export const Header: NextComponentType = () => {
+export function Header() {
   return (
     <div className='shadow-sm border-b bg-white sticky top-0 z-50'>
       <div className='flex justify-between max-w-6xl mx-5 lg:mx-auto'>
