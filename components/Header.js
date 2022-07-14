0e2b0a61a99d33d6d9a47efcd096238996076809
multiahfoon <multiahfoon@gmail.com>
2022-07-14 12:42:45 +1200

    feat: add search bar styling

--- components/Header.js
+++ components/Header.js
@@ -13,7 +13,7 @@ export function Header() {
     <div className='flex justify-between max-w-6xl'>
       {/* left */}
 
-      <div className='relative hidden lg:inline-grid h-24 w-24'>
+      <div className='relative hidden lg:inline-grid w-24'>
         <Image
           layout='fill'
           objectFit='contain'
@@ -21,7 +21,7 @@ export function Header() {
         />
       </div>
 
-      <div className='relative lg:hidden h-10 w-10 flex-shrink-0 cursor-pointer'>
+      <div className='relative lg:hidden w-10 flex-shrink-0 cursor-pointer'>
         <Image
           layout='fill'
           objectFit='contain'
@@ -30,9 +30,16 @@ export function Header() {
       </div>
 
       {/* mid - search bar*/}
-      <div>
-        <SearchIcon className='h-5 w-5 text-gray-500' />
-        <input type='text' placeholder='Search' />
+      <div className='relative mt-1 p-3 rounded-md'>
+        <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
+          <SearchIcon className='h-5 w-5 text-gray-500 ' />
+        </div>
+
+        <input
+          className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md'
+          placeholder='Search'
+          type='text'
+        />
       </div>
 
       {/* right */}
