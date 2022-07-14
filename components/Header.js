9bea3f7dd5b474c94737cc4d3271343e6467c471
multiahfoon <multiahfoon@gmail.com>
2022-07-14 12:12:16 +1200

    feat: add brand name icon on lag screen

--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,14 @@ export function Header() {
     <div className='flex justify-between max-w-6xl'>
       {/* left */}
 
+      <div className='relative hidden lg:inline-grid h-24 w-24'>
+        <Image
+          layout='fill'
+          objectFit='contain'
+          src='https://links.papareact.com/ocw'
+        />
+      </div>
+
       <div className='relative lg:hidden h-10 w-10 flex-shrink-0 cursor-pointer'>
         <Image
           layout='fill'
