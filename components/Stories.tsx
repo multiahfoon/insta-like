52031422277cae3031b7878c7f1e274e548ab58b
multiahfoon <multiahfoon@gmail.com>
2022-07-14 16:23:09 +1200

    wip: import Story component and types

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -1,35 +1,37 @@
 import { faker } from '@faker-js/faker'
-import { NextComponentType } from 'next'
 import { useEffect, useState } from 'react'
+import { Suggestion } from '../types'
+import { Story } from './Story'
 
-interface Suggestion {
-  address: string
-  avatar: string
-  birthdate: string
-  email: string
-  name: string
-  password: string
-  userId: string
-  username: string
-}
-
-export const Stories: NextComponentType = () => {
+export const Stories = () => {
   const [suggestions, setSuggestions] = useState<Suggestion[] | null>()
 
   useEffect(() => {
-    setSuggestions(
-      [...Array(20)].map(() => ({
-        address: faker.address.street(),
-        avatar: faker.image.avatar(),
-        birthdate: faker.date.birthdate().toString(),
-        email: faker.internet.email(),
-        name: faker.name.firstName(),
-        password: faker.internet.password(),
-        userId: faker.datatype.uuid(),
-        username: faker.internet.userName(),
-      }))
-    )
+    if (!suggestions) {
+      setSuggestions(
+        [...Array(20)].map((_, index) => ({
+          address: faker.address.street(),
+          avatar: faker.image.avatar(),
+          birthdate: faker.date.birthdate().toString(),
+          email: faker.internet.email(),
+          id: index.toString(),
+          name: faker.name.firstName(),
+          password: faker.internet.password(),
+          username: faker.internet.userName(),
+        }))
+      )
+    }
   }, [])
 
-  return <div></div>
+  return (
+    <div>
+      {suggestions?.map((profile) => (
+        <Story
+          key={profile.id}
+          imgUrl={profile.avatar}
+          username={profile.username}
+        />
+      ))}
+    </div>
+  )
 }
