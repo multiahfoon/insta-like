536ef6cb2580b64a5f86f775372aa3dcf2e0a844
multiahfoon <multiahfoon@gmail.com>
2022-07-14 16:57:04 +1200

    wip: style user photos

--- components/Story.tsx
+++ components/Story.tsx
@@ -6,8 +6,12 @@ interface Props {
 export function Story({ imgUrl, username }: Props) {
   return (
     <div>
-      <img src={imgUrl} alt='profile picture' />
-      <p>{username}</p>
+      <img
+        className='h-14 w-14 rounded-full p-[1.5px] border-red-500 border-2 object-contain cursor-pointer hover:scale-110 transition transform duration-200 ease-out'
+        src={imgUrl}
+        alt='profile picture'
+      />
+      <p className='text-xs w-14 truncate text-center transform '>{username}</p>
     </div>
   )
 }
