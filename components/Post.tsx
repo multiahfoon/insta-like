e54cfc040b173436c6b06154b3a80948fac6d9ea
multiahfoon <multiahfoon@gmail.com>
2022-07-15 12:48:16 +1200

    feat: add header to post

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,3 +1,36 @@
+import {
+  BookmarkIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  EmojiHappyIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+} from '@heroicons/react/outline'
+import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid'
+
 export function Post({ id, username, userImg, img, caption }) {
-  return <div>{}</div>
+  return (
+    <div className='bg-white my-7 border rounded-sm'>
+      {/* header */}
+      <div className='flex items-center p-5'>
+        <img
+          alt='post profile photo'
+          className='rounded-full h-12 w-12 object-contain border p-1 mr-3'
+          src={userImg}
+        />
+        <p className='flex-1 font-bold'>{username}</p>
+        <DotsHorizontalIcon className='h-5' />
+      </div>
+
+      {/* img */}
+
+      {/* buttons */}
+
+      {/* caption */}
+
+      {/* comments */}
+
+      {/* input box */}
+    </div>
+  )
 }
