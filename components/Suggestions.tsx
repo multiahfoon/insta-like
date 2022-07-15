823d25c0aeca521b021b0fba87b924d65a9454b9
multiahfoon <multiahfoon@gmail.com>
2022-07-15 13:49:21 +1200

    wip: add random user info for suggested users

--- components/Suggestions.tsx
+++ components/Suggestions.tsx
@@ -1,3 +1,26 @@
+import { faker } from '@faker-js/faker'
+import { useEffect, useState } from 'react'
+import { Suggestion } from '../types'
+
 export function Suggestions() {
+  const [suggestions, setSuggestions] = useState<Suggestion[] | null>()
+
+  useEffect(() => {
+    if (!suggestions) {
+      setSuggestions(
+        [...Array(20)].map((_, index) => ({
+          address: faker.address.street(),
+          avatar: faker.image.avatar(),
+          birthdate: faker.date.birthdate().toString(),
+          email: faker.internet.email(),
+          id: faker.datatype.uuid(),
+          name: faker.name.firstName(),
+          password: faker.internet.password(),
+          username: faker.internet.userName(),
+        }))
+      )
+    }
+  }, [])
+
   return <div></div>
 }
