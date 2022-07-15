46dddbc1cce774d583fff587c5c769d0662c1cc5
multiahfoon <multiahfoon@gmail.com>
2022-07-15 14:04:50 +1200

    wip: add suggested users profile image

--- components/Suggestions.tsx
+++ components/Suggestions.tsx
@@ -8,7 +8,7 @@ export function Suggestions() {
   useEffect(() => {
     if (!suggestions) {
       setSuggestions(
-        [...Array(20)].map((_, index) => ({
+        [...Array(4)].map(() => ({
           address: faker.address.street(),
           avatar: faker.image.avatar(),
           birthdate: faker.date.birthdate().toString(),
@@ -22,5 +22,25 @@ export function Suggestions() {
     }
   }, [])
 
-  return <div></div>
+  return (
+    <div className='mt-4 ml-10'>
+      <div className='flex justify-between text-sm mb-5'>
+        <h3 className=' text-sm font-bold text-gray-400'>
+          Suggestions for you
+        </h3>
+        <button className='text-gray-600 font-semibold'>See All</button>
+      </div>
+
+      {/* @TODO: convert following section to component */}
+      {suggestions?.map((profile, index) => (
+        <div key={index} className='flex items-center justify-between mt-3'>
+          <img
+            className='w-10 h-10 rounded-full border p-[2px]'
+            src={profile.avatar}
+            alt='suggested profile photo'
+          />
+        </div>
+      ))}
+    </div>
+  )
 }
