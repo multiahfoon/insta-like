fb78d87dfe19fa48286b3cdf565d0dabfddcc3e1
multiahfoon <multiahfoon@gmail.com>
2022-07-15 12:02:50 +1200

    wip: style scrollbar

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -24,7 +24,7 @@ export const Stories = () => {
   }, [])
 
   return (
-    <div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll'>
+    <div className='flex space-x-2 p-6 bg-white mt-8 border-gray-20 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black'>
       {suggestions?.map((profile) => (
         <Story
           key={profile.id}
