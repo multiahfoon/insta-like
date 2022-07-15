bebf94345e4e61eb4e27fc794c749e21c50b937d
multiahfoon <multiahfoon@gmail.com>
2022-07-15 12:02:02 +1200

    fix: feed stories not centering

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -2,7 +2,7 @@ import { Stories } from './Stories'
 
 export function Feed() {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl max-auto'>
+    <div className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto'>
       <section className='col-span-2'>
         <Stories />
         {/* post */}
