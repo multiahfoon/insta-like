0727a8d9d06ca2d8fffa6066910964caa4cc7b69
multiahfoon <multiahfoon@gmail.com>
2022-07-15 15:19:47 +1200

    chore: remove comments

--- firebase.ts
+++ firebase.ts
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
