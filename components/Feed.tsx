b48af7914fa680c79b4c7dfc7e47c9584328c32c
multiahfoon <multiahfoon@gmail.com>
2022-07-16 16:21:03 +1200

    wip: conditionally toggle components if signed in

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,9 +1,12 @@
+import { useSession } from 'next-auth/react'
 import { MiniProfile } from './MiniProfile'
 import { Posts } from './Posts'
 import { Stories } from './Stories'
 import { Suggestions } from './Suggestions'
 
 export function Feed() {
+  const { data: session } = useSession()
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto'>
       <section className='col-span-2'>
@@ -11,13 +14,14 @@ export function Feed() {
         <Posts />
         {/* post */}
       </section>
-
-      <section className='hidden xl:inline-grid md:col-span-1'>
-        <div className='fixed top-20'>
-          <MiniProfile />
-          <Suggestions />
-        </div>
-      </section>
+      {session && (
+        <section className='hidden xl:inline-grid md:col-span-1'>
+          <div className='fixed top-20'>
+            <MiniProfile />
+            <Suggestions />
+          </div>
+        </section>
+      )}
     </div>
   )
 }
