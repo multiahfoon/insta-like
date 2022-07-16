7a37a54da7e99c0a7bb817648f14fa8e03d7d211
multiahfoon <multiahfoon@gmail.com>
2022-07-16 16:24:01 +1200

    wip: center posts and stories if session false

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -8,11 +8,14 @@ export function Feed() {
   const { data: session } = useSession()
 
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto'>
+    <div
+      className={`grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto ${
+        !session && '!grid-cols-1 !max-w-3xl'
+      }`}
+    >
       <section className='col-span-2'>
         <Stories />
         <Posts />
-        {/* post */}
       </section>
       {session && (
         <section className='hidden xl:inline-grid md:col-span-1'>
