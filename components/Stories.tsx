50aed446171d49e2ce5f711100ca84cb1a71a133
multiahfoon <multiahfoon@gmail.com>
2022-07-16 17:04:28 +1200

    wip: display user info using session

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -1,20 +1,23 @@
 import { faker } from '@faker-js/faker'
+import { useSession } from 'next-auth/react'
 import { useEffect, useState } from 'react'
 import { Suggestion } from '../types'
 import { Story } from './Story'
 
 export const Stories = () => {
+  const { data: session } = useSession()
   const [suggestions, setSuggestions] = useState<Suggestion[] | null>()
 
   useEffect(() => {
     if (!suggestions) {
       setSuggestions(
-        [...Array(20)].map((_, index) => ({
+        [...Array(20)].map(() => ({
           address: faker.address.street(),
           avatar: faker.image.avatar(),
           birthdate: faker.date.birthdate().toString(),
+          company: faker.company.companyName(),
           email: faker.internet.email(),
-          id: index.toString(),
+          id: faker.datatype.uuid(),
           name: faker.name.firstName(),
           password: faker.internet.password(),
           username: faker.internet.userName(),
@@ -25,6 +28,13 @@ export const Stories = () => {
 
   return (
     <div className='flex space-x-2 p-6 bg-white mt-8 border-gray-20 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black'>
+      {session?.user && (
+        <Story
+          key={session.user}
+          imgUrl={session.user.image}
+          username={session.user.username}
+        />
+      )}
       {suggestions?.map((profile) => (
         <Story
           key={profile.id}
