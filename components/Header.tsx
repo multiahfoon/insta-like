e9bf2b5ade64ef7485a0666b6a13933c859724fb
multiahfoon <multiahfoon@gmail.com>
2022-07-16 14:48:39 +1200

    feat: add sign in and sign out on profile click

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,8 +10,11 @@ import {
 } from '@heroicons/react/outline'
 import { HomeIcon } from '@heroicons/react/solid'
 import { useMemo } from 'react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export function Header() {
+  const { data: session } = useSession()
+
   const imgUrl = useMemo(() => {
     return faker.image.avatar()
   }, [])
@@ -51,26 +54,32 @@ export function Header() {
           </div>
         </div>
 
-        {/* right */}
         <div className='flex items-center justify-end space-x-4'>
           <HomeIcon className='navBtn' />
           <MenuIcon className='h-6 md:hidden cursor-pointer' />
 
-          <div className='relative navBtn'>
-            <PaperAirplaneIcon className='navBtn rotate-45' />
-            <div className='absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white'>
-              3
-            </div>
-          </div>
+          {session ? (
+            <>
+              <div className='relative navBtn'>
+                <PaperAirplaneIcon className='navBtn rotate-45' />
+                <div className='absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white'>
+                  3
+                </div>
+              </div>
 
-          <PlusCircleIcon className='navBtn' />
-          <HeartIcon className='navBtn' />
+              <PlusCircleIcon className='navBtn' />
+              <HeartIcon className='navBtn' />
 
-          <img
-            className='h-10 w-10 rounded-full cursor-pointer'
-            alt='profile picture'
-            src={imgUrl}
-          />
+              <img
+                className='h-10 w-10 rounded-full cursor-pointer'
+                alt='profile picture'
+                src={session?.user?.image ? session.user?.image : imgUrl}
+                onClick={signOut}
+              />
+            </>
+          ) : (
+            <button onClick={signIn}>Sign in</button>
+          )}
         </div>
       </div>
     </div>
