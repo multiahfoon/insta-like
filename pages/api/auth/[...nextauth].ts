21113db20ed2c523555e8eb0a9626c38eb2bfa9e
multiahfoon <multiahfoon@gmail.com>
2022-07-16 14:24:04 +1200

    wip: add session callback

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,14 @@ import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
+  callbacks: {
+    async session({ session }) {
+      return session
+    },
+  },
+  pages: {
+    signIn: '/auth/signin',
+  },
   providers: [
     GoogleProvider({
       // wrap env values to work around clientId and clientSecret type bug
@@ -9,7 +17,4 @@ export default NextAuth({
       clientSecret: `${process.env.GOOGLE_SECRET}`,
     }),
   ],
-  pages: {
-    signIn: '/auth/signin',
-  },
 })
