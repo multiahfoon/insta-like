fc5de05f77ab24c97fc1bf174f857067bcfe820b
multiahfoon <multiahfoon@gmail.com>
2022-07-16 16:41:46 +1200

    feat: display user info from session

--- components/MiniProfile.tsx
+++ components/MiniProfile.tsx
@@ -1,24 +1,25 @@
 import { faker } from '@faker-js/faker'
+import { signOut, useSession } from 'next-auth/react'
 import { useMemo } from 'react'
 
 export function MiniProfile() {
-  const currentUser = useMemo(() => {
-    return { username: faker.internet.userName(), img: faker.image.avatar() }
-  }, [])
+  const { data: session } = useSession()
 
   return (
     <div className='flex items-center justify-between mt-14 ml-10'>
       <img
         alt='mini profile photo'
         className='w-16 h-15 rounded-full border p-[2px]'
-        src={currentUser.img}
+        src={session?.user?.image}
       />
       <div className='flex-1 mx-4'>
-        <h2 className='font-bold'>{currentUser.username}</h2>
+        <h2 className='font-bold'>{session?.user?.username}</h2>
         <h3 className='text-sm text-gray-400'>Welcome to Instagram</h3>
       </div>
 
-      <button className='text-blue-400 text-sm font-semibold'>Sign Out</button>
+      <button className='text-blue-400 text-sm font-semibold' onClick={signOut}>
+        Sign Out
+      </button>
     </div>
   )
 }
