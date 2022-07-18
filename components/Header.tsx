191a0e89e7b3e81d28e10d745f619f8df15dab7a
multiahfoon <multiahfoon@gmail.com>
2022-07-18 15:29:56 +1200

    wip: add home router for icons brand and home

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,9 +11,11 @@ import {
 import { HomeIcon } from '@heroicons/react/solid'
 import { useMemo } from 'react'
 import { signIn, signOut, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
 export function Header() {
   const { data: session } = useSession()
+  const router = useRouter()
 
   const imgUrl = useMemo(() => {
     return faker.image.avatar()
@@ -23,7 +25,10 @@ export function Header() {
     <div className='shadow-sm border-b bg-white sticky top-0 z-50'>
       <div className='flex justify-between max-w-6xl mx-5 lg:mx-auto'>
         {/* left */}
-        <div className='relative hidden lg:inline-grid w-24'>
+        <div
+          className='relative hidden lg:inline-grid w-24'
+          onClick={() => router.push('/')}
+        >
           <Image
             layout='fill'
             objectFit='contain'
@@ -31,7 +36,10 @@ export function Header() {
           />
         </div>
 
-        <div className='relative lg:hidden w-10 flex-shrink-0 cursor-pointer'>
+        <div
+          className='relative lg:hidden w-10 flex-shrink-0 cursor-pointer'
+          onClick={() => router.push('/')}
+        >
           <Image
             layout='fill'
             objectFit='contain'
@@ -40,7 +48,6 @@ export function Header() {
         </div>
 
         <div className='max-w-xs'>
-          {/* mid - search bar*/}
           <div className='relative mt-1 p-3 rounded-md'>
             <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
               <SearchIcon className='h-5 w-5 text-gray-500 ' />
@@ -55,8 +62,11 @@ export function Header() {
         </div>
 
         <div className='flex items-center justify-end space-x-4'>
-          <HomeIcon className='navBtn' />
-          <MenuIcon className='h-6 md:hidden cursor-pointer' />
+          <HomeIcon className='navBtn' onClick={() => router.push('/')} />
+          <MenuIcon
+            className='h-6 md:hidden cursor-pointer'
+            onClick={() => router.push('/')}
+          />
 
           {session ? (
             <>
