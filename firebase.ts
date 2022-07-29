c2d7f48ef62d9c762699a5f10cda70239b302c59
multiahfoon <multiahfoon@gmail.com>
2022-07-29 14:48:47 +1200

    fix: firebase database undefined
    Due to firebase config values being stored as environment variables.
    The firebase function that are called on the client side could not
    read the env values.
    
    Current work around is to set env values to public.

--- firebase.ts
+++ firebase.ts
@@ -2,18 +2,19 @@ import { getApp, getApps, initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
+// @TODO: remove public variables and move requests to firebase to custom api route
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
 
 // Check if app is already initialized else get current instance of app
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
-const db = getFirestore()
-const storage = getStorage()
 
+const db = getFirestore(app)
+const storage = getStorage(app)
 export { app, db, storage }
