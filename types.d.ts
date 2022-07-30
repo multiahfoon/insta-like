e99dd38afcf45ca88f1a990753053d321d922e69
multiahfoon <multiahfoon@gmail.com>
2022-07-30 15:05:30 +1200

    wip: add types related to post component

--- types.d.ts
+++ types.d.ts
@@ -1,23 +1,39 @@
-export interface Suggestion {
-  address: string
-  avatar: string
-  birthdate: string
-  company: string
-  email: string
-  id: string
-  name: string
-  password: string
-  username: string
+export type Comment = QueryDocumentSnapshot<DocumentData>[] | null
+export type HasLikes =
+	| QueryDocumentSnapshot<DocumentData>[]
+	| boolean
+	| null
+	| undefined
+export type Likes = QueryDocumentSnapshot<DocumentData>[] | null
+
+export interface Post {
+	id: string
+	username: string
+	userImg: string
+	img: string
+	caption: string
 }
 
 export interface Providers {
-  providers: {
-    google: {
-      id: string
-      name: string
-      type: string
-      signinUrl: string
-      callbackUrl: string
-    }
-  }
+	providers: {
+		google: {
+			id: string
+			name: string
+			type: string
+			signinUrl: string
+			callbackUrl: string
+		}
+	}
+}
+
+export interface Suggestion {
+	address: string
+	avatar: string
+	birthdate: string
+	company: string
+	email: string
+	id: string
+	name: string
+	password: string
+	username: string
 }
