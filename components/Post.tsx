a6b9e8d5fcb508fcfc2b91debfc560fde1e02c5c
multiahfoon <multiahfoon@gmail.com>
2022-07-30 13:02:50 +1200

    wip: toggle post ui only if user is signed in

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,55 +1,59 @@
 import {
-  BookmarkIcon,
-  ChatIcon,
-  DotsHorizontalIcon,
-  EmojiHappyIcon,
-  HeartIcon,
-  PaperAirplaneIcon,
+	BookmarkIcon,
+	ChatIcon,
+	DotsHorizontalIcon,
+	EmojiHappyIcon,
+	HeartIcon,
+	PaperAirplaneIcon,
 } from '@heroicons/react/outline'
 import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid'
+import { useSession } from 'next-auth/react'
 
 export function Post({ id, username, userImg, img, caption }) {
-  return (
-    <div className='bg-white my-7 border rounded-sm'>
-      {/* header */}
-      <div className='flex items-center p-5'>
-        <img
-          alt='post profile photo'
-          className='rounded-full h-12 w-12 object-contain border p-1 mr-3'
-          src={userImg}
-        />
-        <p className='flex-1 font-bold'>{username}</p>
-        <DotsHorizontalIcon className='h-5' />
-      </div>
+	const { data: session } = useSession()
 
-      <img alt='post image' className='object-cover w-full' src={img} />
+	return (
+		<div className='bg-white my-7 border rounded-sm'>
+			{/* header */}
+			<div className='flex items-center p-5'>
+				<img
+					alt='post profile photo'
+					className='rounded-full h-12 w-12 object-contain border p-1 mr-3'
+					src={userImg}
+				/>
+				<p className='flex-1 font-bold'>{username}</p>
+				{session && <DotsHorizontalIcon className='h-5' />}
+			</div>
 
-      <div className='flex justify-between pb-4 pt-4'>
-        <div className='flex space-x-4'>
-          <HeartIcon className='btn' />
-          {/* <HeartIconFilled className='btn' /> */}
-          <ChatIcon className='btn' />
-          <PaperAirplaneIcon className='btn' />
-        </div>
-        <BookmarkIcon className='btn' />
-      </div>
+			<img alt='post image' className='object-cover w-full' src={img} />
+			{session && (
+				<div className='flex justify-between pb-4 pt-4'>
+					<div className='flex space-x-4'>
+						<HeartIcon className='btn' />
+						{/* <HeartIconFilled className='btn' /> */}
+						<ChatIcon className='btn' />
+						<PaperAirplaneIcon className='btn' />
+					</div>
+					<BookmarkIcon className='btn' />
+				</div>
+			)}
 
-      <p className='p-5 truncate'>
-        <span className='font-bold mr-1'>{username}</span>
-        {caption}
-      </p>
+			<p className='p-5 truncate'>
+				<span className='font-bold mr-1'>{username}</span>
+				{caption}
+			</p>
 
-      {/* comments */}
-
-      <form className='flex items-center p-4'>
-        <EmojiHappyIcon className='h-7 ' />
-        <input
-          className='border-none flex-1 focus:ring-0'
-          placeholder='Add a comment...'
-          type='text'
-        />
-        <button className='font-semibold text-blue-400'>Post</button>
-      </form>
-    </div>
-  )
+			{session && (
+				<form className='flex items-center p-4'>
+					<EmojiHappyIcon className='h-7 ' />
+					<input
+						className='border-none flex-1 focus:ring-0'
+						placeholder='Add a comment...'
+						type='text'
+					/>
+					<button className='font-semibold text-blue-400'>Post</button>
+				</form>
+			)}
+		</div>
+	)
 }
