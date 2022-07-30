ca57eabad93b2d89dec2646bed0b8efcd88201f7
multiahfoon <multiahfoon@gmail.com>
2022-07-30 12:39:28 +1200

    feat: fetch posts from db on mount

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,32 +1,34 @@
 import { faker } from '@faker-js/faker'
-import { useMemo } from 'react'
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+import { useEffect, useMemo, useState } from 'react'
+import { db } from '../firebase'
 import { Post } from './Post'
 
 export function Posts() {
-  const MOCK_POST_DATA = useMemo(() => {
-    return [...Array(5)].map(() => {
-      return {
-        id: faker.datatype.uuid(),
-        username: faker.internet.userName(),
-        userImg: faker.image.avatar(),
-        img: faker.image.nature(500, 500, true),
-        caption: faker.lorem.sentence(),
-      }
-    })
-  }, [])
+	const [posts, setPosts] = useState([])
 
-  return (
-    <div>
-      {MOCK_POST_DATA?.map((post) => (
-        <Post
-          key={post.id}
-          id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
-          caption={post.caption}
-        />
-      ))}
-    </div>
-  )
+	useEffect(() => {
+		onSnapshot(
+			query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
+			(snapshot) => {
+				setPosts(snapshot.docs)
+			}
+		)
+	}, [db])
+
+	return (
+		<div>
+			{posts &&
+				posts.map((post) => (
+					<Post
+						key={post.data().timestamp}
+						id={post.data().timestamp}
+						username={post.data().username}
+						userImg={post.data().profileImg}
+						img={post.data().image}
+						caption={post.data().caption}
+					/>
+				))}
+		</div>
+	)
 }
