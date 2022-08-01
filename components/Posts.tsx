7f3ec0671610b821cb76ec5af592d976ab7533d3
multiahfoon <multiahfoon@gmail.com>
2022-08-02 11:35:09 +1200

    wip: remove client side post fetching

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,33 +1,37 @@
 import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
 import { useEffect, useMemo, useState } from 'react'
+import { useRecoilValue } from 'recoil'
+import { postsState } from '../atoms/postsAtom'
 import { db } from '../firebase'
+import { FakePost } from './FakePost'
 import { Post } from './Post'
 
 export function Posts() {
-	const [posts, setPosts] = useState([])
-
-	useEffect(() => {
-		onSnapshot(
-			query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
-			(snapshot: any) => {
-				setPosts(snapshot.docs)
-			}
-		)
-	}, [db])
+	const { fakePosts, posts } = useRecoilValue<any>(postsState)
 
 	return (
 		<div>
-			{posts &&
-				posts.map((post: any) => (
-					<Post
-						key={post.id}
-						id={post.id}
-						username={post.data().username}
-						userImg={post.data().profileImg}
-						img={post.data().image}
-						caption={post.data().caption}
-					/>
-				))}
+			{posts?.length
+				? posts.map((post: any) => (
+						<Post
+							key={post.id}
+							id={post.id}
+							username={post.data().username}
+							userImg={post.data().profileImg}
+							img={post.data().image}
+							caption={post.data().caption}
+						/>
+				  ))
+				: fakePosts?.map((post: any) => (
+						<FakePost
+							key={post.id}
+							id={post.id}
+							username={post.username}
+							userImg={post.profileImg}
+							img={post.image}
+							caption={post.caption}
+						/>
+				  ))}
 		</div>
 	)
 }
