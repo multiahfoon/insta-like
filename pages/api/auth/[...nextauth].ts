7e5bffbcfcd6952284d9ab1faf74f4f61fe00bcd
multiahfoon <multiahfoon@gmail.com>
2022-08-01 15:11:24 +1200

    fix: session type error due to mutation

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,26 +2,20 @@ import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
-  callbacks: {
-    async session({ session, token }) {
-      return {
-        ...session,
-        user: {
-          ...session.user,
-          username: session?.user?.name?.split(' ').join('').toLowerCase(),
-          uuid: token.sub,
-        },
-      }
-    },
-  },
-  pages: {
-    signIn: '/auth/signin',
-  },
-  providers: [
-    GoogleProvider({
-      // wrap env values to work around clientId and clientSecret type bug
-      clientId: `${process.env.GOOGLE_CLIENT_ID}`,
-      clientSecret: `${process.env.GOOGLE_SECRET}`,
-    }),
-  ],
+	callbacks: {
+		async session({ session, token }) {
+			session.token = token.sub
+			return session
+		},
+	},
+	pages: {
+		signIn: '/auth/signin',
+	},
+	providers: [
+		GoogleProvider({
+			// wrap env values to work around clientId and clientSecret type bug
+			clientId: `${process.env.GOOGLE_CLIENT_ID}`,
+			clientSecret: `${process.env.GOOGLE_SECRET}`,
+		}),
+	],
 })
