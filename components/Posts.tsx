22db6c434c6a37878a79b7bc63998e5051eff051
multiahfoon <multiahfoon@gmail.com>
2022-08-24 09:56:24 +1200

    chore: remove imports not being used

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,37 +1,34 @@
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
-import { useEffect, useMemo, useState } from 'react'
 import { useRecoilValue } from 'recoil'
 import { postsState } from '../atoms/postsAtom'
-import { db } from '../firebase'
 import { FakePost } from './FakePost'
 import { Post } from './Post'
 
 export function Posts() {
-	const { fakePosts, posts } = useRecoilValue<any>(postsState)
+  const { fakePosts, posts } = useRecoilValue<any>(postsState)
 
-	return (
-		<div>
-			{posts?.length
-				? posts.map((post: any) => (
-						<Post
-							key={post.id}
-							id={post.id}
-							username={post.data().username}
-							userImg={post.data().profileImg}
-							img={post.data().image}
-							caption={post.data().caption}
-						/>
-				  ))
-				: fakePosts?.map((post: any) => (
-						<FakePost
-							key={post.id}
-							id={post.id}
-							username={post.username}
-							userImg={post.profileImg}
-							img={post.image}
-							caption={post.caption}
-						/>
-				  ))}
-		</div>
-	)
+  return (
+    <div>
+      {posts?.length
+        ? posts.map((post: any) => (
+            <Post
+              key={post.id}
+              id={post.id}
+              username={post.data().username}
+              userImg={post.data().profileImg}
+              img={post.data().image}
+              caption={post.data().caption}
+            />
+          ))
+        : fakePosts?.map((post: any) => (
+            <FakePost
+              key={post.id}
+              id={post.id}
+              username={post.username}
+              userImg={post.profileImg}
+              img={post.image}
+              caption={post.caption}
+            />
+          ))}
+    </div>
+  )
 }
